9c343e142a14ea30b22eebf535cf73b75ce1d4fa
GauravMahajan <gauravmahajan143@gmail.com>
2025-12-20 13:13:17 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
  site: "https://gaurav-RM.github.io",
+ base: '/gaurav-RM.github.io',
   integrations: [icon()],
   output: "server",
 
