6f6c8db3878a0d3b93f3d87109e770ebf742eaaf
GauravMahajan <gauravmahajan143@gmail.com>
2025-12-20 12:53:48 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
-import github from "@astrojs/github";
-import tailwindcss from "@tailwindcss/vite";
+mport tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://gaurav-rm.github.io/",
